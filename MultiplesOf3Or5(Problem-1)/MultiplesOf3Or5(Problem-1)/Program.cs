561a632d918064137c7375402d9cc31746341f19
Yasin Ertekin <111593895+yasinertekin@users.noreply.github.com>
2023-02-06 21:49:56 +0300

    Add files via upload

---
+++ MultiplesOf3Or5(Problem-1)/MultiplesOf3Or5(Problem-1)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ÜcBes = new ArrayList();
+            int toplam = 0;
+            int sayi = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                //Console.WriteLine(sayi);
+                
+
+                if (sayi % 3 == 0 ||sayi % 5 == 0)
+                {
+                    ÜcBes.Add(sayi);
+                }
+                sayi++;
+            }
+
+            //foreach (int item in ÜcBes)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            foreach (int item in ÜcBes)
+            {
+                toplam = toplam + item;
+            }
+
+            Console.WriteLine(toplam);
+            
+        }
+    }
+}
